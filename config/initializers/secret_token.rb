47bb4901281838e39bd2704f1584c7ff4ee67477
Manish Johari <manish.johari@kiwitech.com>
2014-03-03 18:06:20 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolMgmnt::Application.config.secret_token = '88babd33f0f6fcbdfb6217ad70915d6fba84c39e28603434c165a6756e013e7a2a43021a6def50c9a3cdccaa109af12de67fdca0fad6b06cbe60e28645395e24'
